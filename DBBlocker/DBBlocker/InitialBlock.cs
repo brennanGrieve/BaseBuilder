cb0b4397956a69fc33354dc5afe1d70fdfb48dd7
Brennan Grieve <brennangrieve45@gmail.com>
2019-07-25 18:35:23 +1200

    Drag + Drop User Controls Defined
    - Added C# Source files to define Custom shapes
    - Added and visually modelled several User Controls
    
    TODO:
    
    - Finish Provisional Visual Modelling for User Controls
    - Implement Visual Level Drag + Drop Functionality On User Controls
    - Place User Controls in Block Panel on MainWindow

---
+++ DBBlocker/DBBlocker/InitialBlock.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace DBBlocker
+{
+    class InitialBlock : Shape
+    {
+        protected override Geometry DefiningGeometry
+        {
+            get {
+                CombinedGeometry newShape = new CombinedGeometry
+                {
+                    Geometry1 = new EllipseGeometry(new Point(600, 150), 100, 100),
+                    Geometry2 = new RectangleGeometry(new Rect(0,0,600,300))
+                };
+                return newShape;
+            }
+        }
+    }
+}
---
+++ DBBlocker/DBBlocker/SubBlock.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace DBBlocker
+{
+    class SubBlock : Shape
+    {
+        protected override Geometry DefiningGeometry
+        {
+            get
+            {
+                CombinedGeometry newShapeComponent = new CombinedGeometry
+                {
+                    GeometryCombineMode = GeometryCombineMode.Union,
+                    Geometry1 = new EllipseGeometry(new Point(600, 150), 100, 100),
+                    Geometry2 = new RectangleGeometry(new Rect(0, 0, 600, 300))
+                };
+                CombinedGeometry finalNewShape = new CombinedGeometry
+                {
+                    GeometryCombineMode = GeometryCombineMode.Exclude,
+                    Geometry1 = newShapeComponent,
+                    Geometry2 = new EllipseGeometry(new Point(0, 150), 100, 100)
+
+                };
+                return finalNewShape;
+            }
+        }
+    }
+}
